4dbd0bc078a748dca429f3e082c99f609250f4d1
slavesword <slaven.vulic@gmail.com>
2024-03-18 15:13:25 +0000

    installing express and mongo

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://slaven:<slaven>@openclassp6.g1s24ns.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+const productRoutes = require('./routes/product');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('images'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
+module.exports = app;
